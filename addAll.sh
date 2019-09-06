daaafcc4508ed76caf416316278fb29015d542cc
Bastian Nordmeyer <bastian@tinkerforge.com>
2019-09-06 08:45:19 +0200

    add 3d data

--- addAll.sh
+++ addAll.sh
@@ -6,3 +6,6 @@ git add *.mod
 git add *.lib
 git add *.mdc
 git add *.kicad_mod
+find . -name "*.wrl" -exec git add {} \;
+find . -name "*.step" -exec git add {} \;
+find . -name "*.FCStd" -exec git add {} \;
